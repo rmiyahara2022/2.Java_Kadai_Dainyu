98939dba6f0b8f8024427df4ae6afcbf4511efec
rmiya <r.miyahara2022@gmail.com>
2023-09-06 21:09:10 +0900

    first commit

---
+++ Kadai_Dainyu.java
@@ -0,0 +1,20 @@
+public class Kadai_Dainyu {
+	public static void main(String args[]) {
+		// 課題1
+		int x = 7;
+		x = x * 3;
+		System.out.println(x);
+		x = x / 2;
+		System.out.println(x);
+		
+		// 課題2
+		x = 19;
+		int y = 23, z = x * y;
+		System.out.println(z);
+		
+		// 課題3
+		String s = "山田";
+		x = 100;
+		System.out.println(s + "は" + x + "歳です。");
+		}
+}
